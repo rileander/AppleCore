c51003bde53d3f4b8b714e59db365f2d0b501ab4
Lord Calkins <rileander@gmail.com>
2019-03-06 13:57:34 -0600

    Initial Commit

---
+++ AppleCore/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  AppleCore
+//
+//  Created by Riley Calkins on 3/6/19.
+//  Copyright © 2019 Riley Calkins. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ AppleCore/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  AppleCore
+//
+//  Created by Riley Calkins on 3/6/19.
+//  Copyright © 2019 Riley Calkins. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
